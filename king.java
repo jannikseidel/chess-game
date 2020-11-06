5ff931911949694bbe45a48952597a476ac4e2b1
Jannik Seidel <seidel.work@mailbox.org>
2020-11-06 15:56:13 +0100

    king's logic implemented, not determining of move is going to be check

--- king.java
+++ king.java
@@ -7,6 +7,25 @@ public class king extends figure {
     // override the abstract class of can move
     @Override
     public boolean canMove(board board, field start, field stop) {
+        // determine if a move is valid
+        if (Math.abs(stop.getCoord().get_x() - start.getCoord().get_x())<= 1 &
+                Math.abs(stop.getCoord().get_y() - start.getCoord().get_y())<= 1)
+        {
+            // check whether stop field is empty or of different color
+            if (!board.boxes[stop.getCoord().get_y()][stop.getCoord().get_x()].getFig().getColor().
+                    equals(board.boxes[start.getCoord().get_y()][start.getCoord().get_y()].getFig().getColor()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
         return false;
     }
 }
