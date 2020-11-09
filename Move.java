6bed7fe2a1d62e904b5b97602d7b3dca11a63e80
Jannik Seidel <seidel.work@mailbox.org>
2020-11-09 10:00:31 +0100

    Started to write the game and move class

--- Move.java
+++ Move.java
@@ -1,2 +1,31 @@
 public class Move {
+
+    // Class representing a chess move
+    private Player player;
+    private field start;
+    private field stop;
+    private figure figureMoved;
+    private figure figureKilled;
+
+    // Constructor of a Move
+    public Move(Player player, field start, field stop){
+        this.player = player;
+        this.start = start;
+        this.stop = stop;
+        this.figureMoved = start.getFig();
+    }
+    // validate move
+    public boolean validMove(board board){
+        return figureMoved.canMove(board, this.start, this.stop);
+
+    }
+    // determine if a figure is killed
+    public void determineFigureKilled(board board){
+        if (validMove(board) & !this.start.getFig().getColor().equals(this.stop.getFig().getColor()) )
+        {
+            this.figureKilled = stop.getFig();
+        }
+
+    }
+
 }
--- game.java
+++ game.java
@@ -1,2 +1,34 @@
+import java.util.List;
+
 public class game {
+    // Player[] to keep track of the two players, are they human or computer, color and so on
+    // board to have a representation of the board
+    // currentTurn to determine which player is next
+    // status to determine current gamestate
+    // movesPlayed to keep track of all played moves
+
+    //TODO Implement check for Check and Check-mate
+
+    private Player[] players;
+    private board board;
+    private Player currentTurn;
+    private GameStatus status;
+    private List<Move> movesPlayed;
+
+    private void initialize (Player p1, Player p2)
+    {
+        players[0] = p1;
+        players[1] = p2;
+        board.resetBoard();
+
+        if (p1.playerColor().equals("white"))
+        {
+            this.currentTurn = p1;
+        }
+        else
+        {
+            this.currentTurn = p2;
+        }
+        movesPlayed.clear();
+    }
 }
